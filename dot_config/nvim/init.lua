3d82a2f24308fa378a3b5ba306e1a72e58749b21
William Ngo <wingo7971@gmail.com>
2026-05-22 15:59:44 -0400

    auto_install=false

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1044,7 +1044,7 @@ require("lazy").setup({
 				"json5",
 			},
 			-- Autoinstall languages that are not installed
-			auto_install = true,
+			auto_install = false,
 			highlight = {
 				enable = true,
 				-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
